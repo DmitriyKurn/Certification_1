80f5bc063c6e782087c5429fb85c4bf4957d9b07
Dmitriy <skverdv@gmail.com>
2023-08-01 18:16:22 +0300

    Entering_array

---
+++ TaskSert/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int m = int.Parse(Console.ReadLine()!);
+string [] stringArray = new string [m];
+void array(string [] stringArray)
+{
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива");
+     stringArray[i] = Console.ReadLine();
+  }
+}
+
